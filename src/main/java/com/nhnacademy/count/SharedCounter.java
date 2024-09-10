ddb4be0c565e4c7b5060a477769efca650d4ecd0
강산천 <ksc4305@ksc4305.local>
2024-09-10 20:25:58 +0900

    CounterIncreaseHandlerSynchronized

--- src/main/java/com/nhnacademy/count/SharedCounter.java
+++ src/main/java/com/nhnacademy/count/SharedCounter.java
@@ -16,29 +16,35 @@ public class SharedCounter {
     private long count;
 
     public SharedCounter(){
-        this(0l);
+        this(0L);
     }
 
     public SharedCounter(long count) {
         //TODO#1-1 생성자를 초기화 합니다. count < 0 IllegalArgumentException아 발생 합니다.
-
+        if(count < 0){
+            throw new IllegalArgumentException();
+        }
         this.count = count;
     }
 
     //TODO#1-2 mehtod 단위 lock을 걸고, count 를 반환 합니다.
-    public long getCount(){
+    public synchronized long getCount(){
         return count;
     }
 
     public long increaseAndGet(){
         //TODO#1-3 block 단위로 lock을 걸고 count = count + 1 증가시키고 count를 반환 합니다.
-        count = count + 1;
+        synchronized (this) {
+            count = count + 1;
+        }
         return count;
     }
 
     public long decreaseAndGet(){
         //TODO#1-4 count = count -1  부분 lock을 걸고, count를 반환 합니다.
-        count = count - 1;
+        synchronized (this) {
+            count = count - 1;
+        }
         return count;
     }
 }
